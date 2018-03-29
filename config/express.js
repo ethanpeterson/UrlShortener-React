1a5a3963b5c06fb8717303feefff37feaa0cd8eb
Ethan Peterson <ethan@kaden.khp>
2018-03-29 17:04:07 -0500

    Updated license statement

--- config/express.js
+++ config/express.js
@@ -1,4 +1,22 @@
-//require('babel-core/register'); // required here for parsing JSX or ES6 files on server side
+/*************************************************************************
+Author: Ethan Peteson
+Created: 30-Aug-2017
+
+License: Apache 2.0 Licensed
+Updated: 28-Dec-2017
+
+Copyright 2017-2018
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*************************************************************************/
+
 require('babel-register'); // required here for parsing JSX or ES6 files on server side
 
 var config = require('./config'),
